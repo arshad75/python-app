03ceb1cebc92b63e9b9c30cd108ccbdecabb607b
arshad75 <arshad.syed75@gmail.com>
2024-07-26 15:00:15 +0000

    tf

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-instance"
+  }
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket"
+  acl    = "public"
+
+  tags = {
+    Name        = "example-bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_security_group" "example" {
+  name_prefix = "example-"
+  vpc_id      = "vpc-123456"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
